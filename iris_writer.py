495602105bb0dd7c6684bc85888f29096c460e89
Aaron Schumacher <ajschumacher@gmail.com>
2015-04-19 12:41:30 -0400

    add index column properly

--- iris_writer.py
+++ iris_writer.py
@@ -6,4 +6,9 @@ iris = datasets.load_iris()
 species = iris.target_names[iris.target]
 iris = pd.DataFrame(np.column_stack((iris.data, species)),
                     columns=iris.feature_names + ['species'])
-iris.reset_index()
+iris.reset_index()              # include ID (index) as data
+iris.to_csv('iris.csv')         # write without internal index
+
+iris.iloc[7, 2] = 1.6
+print iris.iloc[7, 2]
+iris.to_csv('iris_one_change.csv', index=False)
