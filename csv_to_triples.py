784e633ed6ba32fa39a96982ceb3e5be6ac231a8
Aaron Schumacher <ajschumacher@gmail.com>
2015-04-26 22:58:36 -0400

    factor out functions; adapt for py3

--- csv_to_triples.py
+++ csv_to_triples.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python
 
-import sys, csv
+import sys
+import dd
 
-# currently loading everything into memory
-# rather than going line-by-line
-
-
-def handle_to_list_of_dicts(handle):
-    reader = csv.DictReader(handle)
-    return reader
-
-
-def list_of_dicts_to_triple_set(lod, id_name):
-    ids = set()
-    triple_set = set()
-    for d in lod:
-        if id_name not in d:
-            raise Exception('entity does not have ID ' + id_name)
-        current_id = d[id_name]
-        if current_id in ids:
-            raise Exception('ID ' + current_id + 'is not unique')
-        ids.add(current_id)
-        for prop, value in d.iteritems():
-            if prop == id_name:
-                continue
-            triple_set.add((current_id, prop, value))
-    return triple_set
 
 if __name__ == '__main__':
     # default to using `index` as ID column for now
-    lod = handle_to_list_of_dicts(sys.stdin)
-    trips = list_of_dicts_to_triple_set(lod, 'index')
+    lod = dd.handle_to_list_of_dicts(sys.stdin)
+    trips = dd.list_of_dicts_to_triple_set(lod, 'index')
     for trip in trips:
-        print trip
+        print(trip)
---
+++ dd.py
@@ -0,0 +1,27 @@
+import csv
+
+
+# currently loading everything into memory
+# rather than going line-by-line
+
+
+def handle_to_list_of_dicts(handle):
+    reader = csv.DictReader(handle)
+    return reader
+
+
+def list_of_dicts_to_triple_set(lod, id_name):
+    ids = set()
+    triple_set = set()
+    for d in lod:
+        if id_name not in d:
+            raise Exception('entity does not have ID ' + id_name)
+        current_id = d[id_name]
+        if current_id in ids:
+            raise Exception('ID ' + current_id + 'is not unique')
+        ids.add(current_id)
+        for prop, value in d.items():
+            if prop == id_name:
+                continue
+            triple_set.add((current_id, prop, value))
+    return triple_set
